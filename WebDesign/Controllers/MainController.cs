16248bdffdeb069512cd5796792488af054f3613
Risa Iizuka <helmet822@gmail.com>
2021-03-20 19:47:17 +0900

    スキャフォールディング

--- WebDesign/Controllers/MainController.cs
+++ WebDesign/Controllers/MainController.cs
@@ -1,18 +1,130 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using MvcMain.Models;
 
 namespace WebDesign.Controllers
 {
     [RoutePrefix("")]
-    public class MainController : Controller {
-        // GET: Main
+    public class MainController : Controller
+    {
+        private MainDBContext db = new MainDBContext();
+
         [Route("")]
         [Route("Index")]
-        public ActionResult Index() {
+        // GET: Main
+        public ActionResult Index()
+        {
+            return View(db.Mains.ToList());
+        }
+
+        // GET: Main/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Main main = db.Mains.Find(id);
+            if (main == null)
+            {
+                return HttpNotFound();
+            }
+            return View(main);
+        }
+
+        // GET: Main/Create
+        public ActionResult Create()
+        {
             return View();
+        }
+
+        // POST: Main/Create
+        // 過多ポスティング攻撃を防止するには、バインド先とする特定のプロパティを有効にしてください。
+        // 詳細については、https://go.microsoft.com/fwlink/?LinkId=317598 をご覧ください。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Date,Price,Genre,Person,Memo")] Main main)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Mains.Add(main);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(main);
+        }
+
+        // GET: Main/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Main main = db.Mains.Find(id);
+            if (main == null)
+            {
+                return HttpNotFound();
+            }
+            return View(main);
+        }
+
+        // POST: Main/Edit/5
+        // 過多ポスティング攻撃を防止するには、バインド先とする特定のプロパティを有効にしてください。
+        // 詳細については、https://go.microsoft.com/fwlink/?LinkId=317598 をご覧ください。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Date,Price,Genre,Person,Memo")] Main main)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(main).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(main);
+        }
+
+        // GET: Main/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Main main = db.Mains.Find(id);
+            if (main == null)
+            {
+                return HttpNotFound();
+            }
+            return View(main);
+        }
+
+        // POST: Main/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Main main = db.Mains.Find(id);
+            db.Mains.Remove(main);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
